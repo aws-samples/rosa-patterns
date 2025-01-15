d5e34a8bb1d06a276532b2c3f775a11fcf6ae2d3
Will Garcia <willgrod@amazon.com>
2025-01-15 15:31:01 +1000

    chore: delete scripts

--- templates/terraform/src/scripts/terraform-docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-for d in . modules/* examples/*; do
-  echo $d
-  rm -rf $d/.terraform $d/.terraform.lock.hcl
-  terraform-docs -c .terraform-docs.yml $d
-done
--- templates/terraform/src/scripts/verify-gen.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -xe
-
-if [[ -n "$(git status --porcelain | grep -v "Dockerfile")" ]]; then
-    echo "It seems like you need to run 'make terraform-docs'. Please run it and commit the changes"
-    git status --porcelain | grep -v "Dockerfile"
-    exit 1
-fi
